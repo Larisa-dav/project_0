75ef568de1029df71ecaa40a51d4a30993ed8cb2
Larisa <l.a.bazhanova@gmail.com>
2021-02-16 16:13:15 +0300

    add README

--- module_0/module_0.py
+++ module_0/module_0.py
@@ -2,8 +2,8 @@ import numpy as np
 
 
 def game_core_v1(number):
-    '''Просто угадываем на random, никак не используя информацию о больше или меньше.
-       Функция принимает загаданное число и возвращает число попыток'''
+    """Просто угадываем на random, никак не используя информацию о больше или меньше.
+       Функция принимает загаданное число и возвращает число попыток"""
     count = 0
     while True:
         count += 1
@@ -13,7 +13,7 @@ def game_core_v1(number):
 
 
 def score_game(game_core):
-    '''Запускаем игру 1000 раз, чтобы узнать, как быстро игра угадывает число'''
+    """Запускаем игру 1000 раз, чтобы узнать, как быстро игра угадывает число"""
     count_ls = []
     np.random.seed(1)  # фиксируем RANDOM SEED, чтобы ваш эксперимент был воспроизводим!
     random_array = np.random.randint(1, 101, size=(1000))
@@ -25,8 +25,8 @@ def score_game(game_core):
 
 
 def game_core_v2(number):
-    '''Сначала устанавливаем любое random число, а потом уменьшаем или увеличиваем его в зависимости от того, больше оно или меньше нужного.
-       Функция принимает загаданное число и возвращает число попыток'''
+    """Сначала устанавливаем любое random число, а потом уменьшаем или увеличиваем его в зависимости от того,
+    больше оно или меньше нужного.Функция принимает загаданное число и возвращает число попыток"""
     count = 1
     predict = np.random.randint(1, 101)
 
@@ -64,7 +64,7 @@ def game_core_v3(number):
 
         predict = np.random.randint(minimum, maximum)
         # print(minimum, maximum, predict, number)
-    return (count)  # выход из цикла, если угадали
+    return count  # выход из цикла, если угадали
 
 
 score_game(game_core_v3)
@@ -88,7 +88,7 @@ def game_core_v4(number):
         count += 1
         # print(minimum, maximum, predict, number)
 
-    return (count)  # выход из цикла, если угадали
+    return count  # выход из цикла, если угадали
 
 
 score_game(game_core_v4)
@@ -111,7 +111,7 @@ def game_core_v5(number):
         predict = int((maximum - minimum) / 2 + minimum)  # всегда дели диапазон пополам
         # print(minimum, maximum, predict, number)
 
-    return (count)  # выход из цикла, если угадали
+    return count  # выход из цикла, если угадали
 
 
 score_game(game_core_v5)
